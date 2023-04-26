01ea1c585e1c728e3703bafd77ccfca6c781fff3
Thalys001 <thalyssouza708@gmail.com>
2023-04-25 23:38:40 -0300

    feat: creating modal form structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-function App() {
-  return (
-   <div>modal form</div>
-  )
-}
-
-export default App
---
+++ src/Modal.jsx
@@ -0,0 +1,39 @@
+import { Fragment, useState } from 'react'
+import './style.css'
+
+const Modal = () => {
+  const [show, setShow] = useState(false);
+
+  const toggle = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <button className='btn-primary' onClick={toggle}> Abrir Modal</button>
+      <div className={`modal ${show && 'show'}`} onClick={toggle}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-header">
+            <h2 className="title">Título</h2>
+            <h2 className="modal-close" onClick={toggle}>
+              x
+            </h2>
+          </div>
+          <div className="modal-body">
+            <div className="form-control">
+              <label>Nome</label>
+              <input type="text" placeholder='Digite seu nome...' />
+              <label>Email</label>
+              <input type="text" placeholder='Digite seu email...' />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button className="btn-cancel">Cancelar</button>
+            <button className="btn-primary">Salvar</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Modal;
